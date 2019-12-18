87903cd1302b1847d7435cbff583e4bf99a0df51
Kelsey Creel <kelseycreel@gmail.com>
2019-12-17 19:48:12 -0600

    read is complete

--- src/Team/Team.js
+++ src/Team/Team.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
+import Player from '../Player/Player';
 import playerData from '../helpers/data/playerData';
 
+import './Team.scss';
+
 class Team extends React.Component {
+  state = {
+    players: [],
+  }
+
+  componentDidMount() {
+    this.getPlayers();
+  }
+
+  getPlayers = () => {
+    playerData.getPlayers()
+      .then((players) => {
+        this.setState({ players });
+      })
+      .catch((error) => console.error(error));
+  }
+
+
   render() {
-    const players = playerData.getPlayers();
     return (
-      <div className="Team">
-        {/* Player cards */}
-        Team
+      <div className="Team d-flex flex-row flex-wrap">
+        {this.state.players.map((player) => (<Player key={player.id} player={player} />))}
       </div>
     );
   }
